d507e176af5c04d69f19742914e477e20c54f75a
Aimee <aimee4312@gmail.com>
2024-01-13 03:57:34 -0800

    made the tutorial animations run concurrently

--- hands.py
+++ hands.py
@@ -1,13 +1,5 @@
 import main
 import pygame
-shoot_sprite = [main.pygame.image.load("sprites/shoot1.png"),
-                main.pygame.image.load("sprites/shoot2.png"),
-                main.pygame.image.load("sprites/shoot3.png"),
-                main.pygame.image.load("sprites/shoot1.png")]
-reload_sprite = [main.pygame.image.load("sprites/reload1.png"),
-                main.pygame.image.load("sprites/reload2.png"),
-                main.pygame.image.load("sprites/reload1.png")]
-shield_sprite = [main.pygame.image.load("sprites/shield.png")]
 
 x = 200
 y = 150
@@ -16,30 +8,27 @@ def hands(selected_choice):
     if selected_choice == "shoot":
         while index < 4:
             main.clock.tick(4)
-            if index >= len(shoot_sprite):
+            if index >= len(main.shoot_sprite):
                 index = 0
 
-            image = shoot_sprite[index]
-            main.window.blit(image, (x, y))
+            main.window.blit(main.shoot_sprite[index], (x, y))
             main.pygame.display.update()
             main.window.fill((0, 0, 0))
             index += 1
     elif selected_choice == "shield":
             main.clock.tick(1)
-            if index >= len(shield_sprite):
+            if index >= len(main.shield_sprite):
                 index = 0
 
-            image = shield_sprite[index]
-            main.window.blit(image, (x, y))
+            main.window.blit(main.shield_sprite[index], (x, y))
             main.pygame.display.update()
     elif selected_choice == "reload":
         while index < 3:
             main.clock.tick(3)
-            if index >= len(reload_sprite):
+            if index >= len(main.reload_sprite):
                 index = 0
 
-            image = reload_sprite[index]
-            main.window.blit(image, (x, y))
+            main.window.blit(main.reload_sprite[index], (x, y))
             main.pygame.display.update()
             main.window.fill((0, 0, 0))
             index += 1
